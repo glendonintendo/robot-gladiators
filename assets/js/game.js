eee2993bcc0f4a35b3153d671d7ffb079d80ef77
glendo <gleneggers64@gmail.com>
2021-02-08 16:30:36 -0600

    add choice to join fight and penalty for skipping fight; add playerMoney global var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerHealth, playerAttack);
 
@@ -10,20 +11,36 @@ var enemyAttack = 12;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
-    enemyHealth -= playerAttack;
-    console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health.`);
-    if (enemyHealth <= 0) {
-        window.alert(`${enemyName} has died!`);
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    console.log(promptFight);
+    if (promptFight.toLowerCase() === 'fight') {
+        enemyHealth -= playerAttack;
+        console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health.`);
+        if (enemyHealth <= 0) {
+            window.alert(`${enemyName} has died!`);
+        } else {
+            window.alert(`${enemyName} still has ${enemyHealth} health left.`);
+        }
+        
+        playerHealth -= enemyAttack;
+        console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health.`);
+        if (playerHealth <= 0) {
+            window.alert(`${playerName} has died!`);
+        } else {
+            window.alert(`${playerName} still has ${playerHealth} health left.`);
+        }
+
+    } else if (promptFight.toLowerCase() === 'skip') {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        if (confirmSkip) {
+            window.alert(`${playerName} has decided to skip this fight. Goodbye!`);
+            playerMoney -= 2;
+        } else {
+            fight();
+        }
+        
     } else {
-        window.alert(`${enemyName} still has ${enemyHealth} health left.`);
-    }
-    
-    playerHealth -= enemyAttack;
-    console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health.`);
-    if (playerHealth <= 0) {
-        window.alert(`${playerName} has died!`)
-    } else {
-        window.alert(`${playerName} still has ${playerHealth} health left.`)
+        window.alert(`You need to choose a valid option. Try again!`);
     }
 };
 
