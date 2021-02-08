24ddd79b2a34cf724122f100432298118e6de0ee
glendo <gleneggers64@gmail.com>
2021-02-08 16:03:43 -0600

    add variables for robot stats; add fgith function to simulate a fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,30 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
-
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
-// fight();
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerHealth, playerAttack);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth -= playerAttack;
+    console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health.`);
+    if (enemyHealth <= 0) {
+        window.alert(`${enemyName} has died!`);
+    } else {
+        window.alert(`${enemyName} still has ${enemyHealth} health left.`);
+    }
+    
+    playerHealth -= enemyAttack;
+    console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health.`);
+    if (playerHealth <= 0) {
+        window.alert(`${playerName} has died!`)
+    } else {
+        window.alert(`${playerName} still has ${playerHealth} health left.`)
+    }
+};
+
+fight();
