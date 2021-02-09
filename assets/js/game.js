45a5c3a2ecb9353a4aa9c6afa447ecfda61e0abc
glendo <gleneggers64@gmail.com>
2021-02-09 11:32:33 -0600

    create getPlayerName function; add function call to player object

--- assets/js/game.js
+++ assets/js/game.js
@@ -99,10 +99,19 @@ var shop = function() {
 var randomNumber = function(min, max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
-}
+};
+
+var getPlayerName = function() {
+    var name = ""
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log(`Your robot name is ${name}`);
+    return name;
+};
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
